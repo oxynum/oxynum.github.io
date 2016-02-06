960f721364f5dcd10117945c85e6c983b751e19b
ylloyd <yvan@oxynum.fr>
2016-02-06 15:16:32 +0100

    Pretty URL HTML History and base URL added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,7 @@ angular
     'ngTouch',
     'duScroll'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -33,4 +33,6 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
   });
