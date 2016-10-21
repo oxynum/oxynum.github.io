5d7b98262a8bad261e3458038d922541f38702a8
THIMOTHEE Rudy <dathims@gmail.com>
2016-10-22 01:37:11 +0200

    change gh page folder

---
+++ scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name oxynum2016App
+ * @description
+ * # oxynum2016App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('oxynum2016App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'duScroll'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
+  });
