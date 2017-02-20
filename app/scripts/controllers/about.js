05ea911fec4c9d5ef21bfe061a85e0a09830eb01
THIMOTHEE Rudy <dathims@gmail.com>
2017-02-20 14:48:37 +0100

    add duplicate content quick and dirty to send my email

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,19 @@
  * Controller of the oxynum2016App
  */
 angular.module('oxynum2016App')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AboutCtrl', ['$scope', '$document', function ($scope, $document) {
+
+  	$scope.pageClass = 'main-view';
+  	$scope.welcome = true;
+    $scope.team = true;
+
+  	if (!sessionStorage.user) {
+    	$scope.welcome = false;
+    };
+    sessionStorage.setItem("user", "on");
+
+	$scope.scrollTo = function(id) {
+		var memberID = document.getElementById(id);
+		$document.scrollToElementAnimated(angular.element(memberID), 0, 1000, null);
+	}
+  }]);
