deab41f7083e6d1aa86d1bca53daba4f4e906cf9
A-Laure_DeV <77326841+AnneLaureSIEVERING@users.noreply.github.com>
2021-10-25 17:57:59 +0200

    homepage -breakpoint mobile & tablet. WIP breakpoint desktop

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+function displayPresentationImage(element) {
+  console.log(element)
+  // if element === truc {}
+  //utiliser le switch 
+  //pointer l'élément img class="section-agency-image--mobile"
+  //element.setAttribute('src', './images/mobile-image-clients/imvitro.jpg');
+}
+
+function displayPresentationVideo(element) {
+  console.log(element)
+  // if element === truc {}
+  //utiliser le switch 
+  //element.setAttribute();
+}
