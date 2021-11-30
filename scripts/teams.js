d46ab48eec9b44badbf397d1e27cbd1cab5ae5d1
A-Laure_DeV <77326841+AnneLaureSIEVERING@users.noreply.github.com>
2021-11-30 17:12:05 +0100

    WIP animation carousel page teams

--- scripts/teams.js
+++ scripts/teams.js
@@ -1,48 +1,17 @@
-const imagePath = './images/photos-teams/';
+var pos = 0;
 
-let imagesTeamList = [
-  'pexels-photo-1181715.jpeg',
-  'maxime.jpg',
-  'thomas.jpg',
-  'maddy.jpg',
-  'hugo.jpg',
-  'jessica.jpg',
-  'jonathan.jpg',
-  'pascal.jpg',
-  'pierrick.jpg',
-  'rudy.jpg',
-  'sandrine.jpg',
-  'pexels-photo-1181715.jpeg'
-];
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// Next controls
+function plusSlides() {
+  showSlides();
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+// Previous controls
+function previousSlide() {
+  document.querySelector(".team-carousel").style.transform = "translateY("+ -pos +"vh)"
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("team-carousel__slide");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  console.log(slides[slideIndex-1]);
-  console.log(slides[slideIndex]);
-  //slides[slideIndex-1].addClassName == 'animation0';
-  slides[slideIndex-1].style.backgroundImage = "url(" + imagePath + imagesTeamList[slideIndex-1] + ")", 
-  slides[slideIndex-1].style.display = "block";
+
+function showSlides() {
+  pos = pos - 100;
+  document.querySelector(".team-carousel").style.transform = "translateY("+ pos +"vh)"
 }
